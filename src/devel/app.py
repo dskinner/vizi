5eb1582746215cf28a7a14a42b96181ca2859117
dasacc22 <dasacc22@gmail.com>
2009-07-11 17:37:04 -0400

    moved www source and deleted a bunch of old stuff

--- src/devel/app.py
+++
@@ -1,4 +0,0 @@
-from PyQt4 import QtGui
-import sys
-
-app = QtGui.QApplication(sys.argv)
--- src/min.py
+++
@@ -1,8 +0,0 @@
-import sndobj
-import time
-
-jack = sndobj.SndJackIO('vizi')
-thread = sndobj.SndThread()
-thread.AddObj(jack, sndobj.SNDIO_OUT)
-thread.ProcOn()
-time.sleep(5)
